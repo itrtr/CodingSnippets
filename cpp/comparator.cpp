4385a08c74e680c6cdb5ce7580ed4611144c058e
Pankaj Singh <theiterator@gmail.com>
2022-12-29 23:55:51 +0530

    update comparator.cpp with new methods

--- cpp/comparator.cpp
+++ cpp/comparator.cpp
@@ -48,3 +48,47 @@ sort(begin(tasks), end(tasks), [](const Task& t1, const Task& t2) {
          return (t1.start == t2.start) ? t1.ptime < t2.ptime : t1.start < t2.start;
      });
 
+
+
+// Vector works with passing a function as well which return bool
+bool VectorComparator(const Task& t1, const Task& t2) {
+    return (t1.start == t2.start) ? t1.ptime < t2.ptime : t1.start < t2.start;
+}
+
+sort(begin(tasks), end(tasks), VectorComparator);
+
+
+------------------------------------------------------------------------------------------------------------------------
+// Usually we would require custom comparator for PQ, Set, Map or STL functions like std::sort()
+
+// As discussed for PQ, Set and Map these too also take such comparator
+std::set<Type, Comparator2> my_set; // similar for Map
+
+// There are new approaches where we don't need to create a struct just to define operator()
+// we can also have a free function which returns bool and we can wrap it
+
+bool CustomComparator(Type a, Type b) { return ....; }
+
+// Before C++ 20, we can decltype on function but we also need to pass the comparator as ctor to the Set/Map/PQ
+priority_queue<Task, vector<Task>, decltype(CustomComparator)*> pq_tasks(CustomComparator); // Either pass pointer or reference to function (functor) and also to ctor of PQ
+set<Task, decltype(&CustomComparator)> ss(&CustomComparator); // Using reference
+
+// But with lambda function we don't need to explict adding "*" or "&"
+auto LambdaCustomComparator = [](Type& a, Type& b) -> bool { return ...; };
+set<Task, decltype(LambdaCustomComparator)> ss(LambdaCustomComparator);
+
+// With C++ 20 (we can avoid passing comparator to ctor of PQ/Set/Map)
+auto LambdaCustomComparator = [](Type& a, Type& b) -> bool { return ...; };
+set<Task, decltype(LambdaCustomComparator)> ss;
+
+
+
+
+
+
+
+
+
+
+
+
